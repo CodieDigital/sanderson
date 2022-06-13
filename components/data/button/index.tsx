c6e5dfbfcac0d8706a99532d247bb04648bdef37
CodieDigital <50671603+CodieDigital@users.noreply.github.com>
2022-06-13 15:28:45 -0300

    Add files via upload

--- components/data/button/index.tsx
+++ components/data/button/index.tsx
@@ -4,13 +4,14 @@ import Link from "next/link";
 interface ButtonInfos {
   href: string;
   text: string;
+  target?: string;
 }
 
-export function ButtonComponent({ href, text }: ButtonInfos) {
+export function ButtonComponent({ href, text, target }: ButtonInfos) {
   return (
     <ButtonStyle>
       <Link href={href} passHref>
-        <a href="replaced" className="paragraph-18-bold">
+        <a href="replaced" className="paragraph-18-bold" target={target}>
           <span>{text}</span>
         </a>
       </Link>
